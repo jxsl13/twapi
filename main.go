6b4d869d954fed43db92989353dff714a9263629
jxsl13 <jxsl13@gmail.com>
2020-01-27 02:31:31 +0100

    example executable

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func getMasterServerList() (masterservers []*MasterServer) {
@@ -18,5 +20,45 @@ func getMasterServerList() (masterservers []*MasterServer) {
 }
 
 func main() {
+	cache := NewConcurrentServerCache(2048)
+
+	ms, _ := NewMasterServerFromAddress("master1.teeworlds.com:8283")
+	srvAddresses, _ := ms.GetServerList()
+
+	fmt.Printf("Retrieved %d server addresses from master server.\n", len(srvAddresses))
+	gameservers := make([]GameServer, 0, len(srvAddresses)*2)
+	for _, addr := range srvAddresses {
+		srv, _ := NewGameServerFromUDPAddr(&addr, 5*time.Second, 5)
+
+		gameservers = append(gameservers, srv)
+	}
+
+	start := func(gs GameServer, c *ConcurrentServerCache, wg *sync.WaitGroup) {
+		info, err := gs.ServerInfo()
+		if err != nil {
+			return
+		}
+		if info.Valid() {
+			cache.Add(&info)
+		}
+		fmt.Printf("Retrieving data from: %s\n", gs.RemoteAddr().String())
+		wg.Done()
+	}
+
+	wg := sync.WaitGroup{}
+
+	lim := 5
+	for _, gs := range gameservers {
+		lim--
+		if lim == 0 {
+			break
+		}
+		if gs.ValidConnection() {
+			wg.Add(1)
+			go start(gs, cache, &wg)
+		}
+	}
+
+	wg.Wait()
 
 }
